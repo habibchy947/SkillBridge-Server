6ee97dcb5fbce610109950aeebac59dcba4f2946
habibchy947 <habibullahalquaderi2005@gmail.com>
2026-02-04 16:55:50 +0600

    implement role based access control

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -1,23 +1,46 @@
 import { Request, Response } from "express";
 import { categoryServices } from "./category.services";
+import { Prisma } from "../../../generated/prisma/client";
+import { UserRole } from "../../middlewares/auth";
 
-const createCategory = async (req: Request, res: Response ) => {
-    try {
-        const result = await categoryServices.createCategory(req.body);
-        return res.status(201).json({
-            success: true,
-            message: "Category created successfully",
-            data: result
-        });
-    } catch (error) {
-        return res.status(400).json({
-            success: false,
-            message: "Category Creation Failed",
-            details: error
-        });
-    };
+const createCategory = async (req: Request, res: Response) => {
+  try {
+    if (!req.user) {
+      return res.status(403).json({
+        success: false,
+        message: "Unauthorized ",
+      });
+    } else if (req.user.role !== UserRole.ADMIN) {
+      return res.status(403).json({
+        success: false,
+        message: "Unauthorized Access",
+      });
+    }
+
+    const result = await categoryServices.createCategory(req.body);
+    return res.status(201).json({
+      success: true,
+      message: "Category created successfully",
+      data: result,
+    });
+  } catch (error) {
+    if (
+      error instanceof Prisma.PrismaClientKnownRequestError &&
+      error.code === "P2002"
+    ) {
+      return res.status(409).json({
+        success: false,
+        message: "Category is already exists",
+      });
+    }
+    return res.status(500).json({
+      success: false,
+      message: "Category Creation Failed",
+      details: error,
+    });
+  }
 };
 
 export const categoryController = {
-    createCategory,
+  createCategory,
 };
--- src/modules/category/category.routes.ts
+++ src/modules/category/category.routes.ts
@@ -1,8 +1,12 @@
 import express, { Router } from "express";
 import { categoryController } from "./category.controller";
+import auth, { UserRole } from "../../middlewares/auth";
+// import { UserRole } from "../../../generated/prisma/enums";
+
 
 const router = express.Router();
 
-router.post("/", categoryController.createCategory);
+
+router.post("/", auth(UserRole.ADMIN), categoryController.createCategory);
 
 export const categoryRouter: Router = router;
